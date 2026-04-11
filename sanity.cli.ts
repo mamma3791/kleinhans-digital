aa8ab859db2c3e090a01f1db5bf3b6141e025c28
mamma3791 <mamm3791@gmail.com>
2026-04-11 22:14:56 +0200

    Add Sanity CLI config, remove client from repo

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "z24f1kn9",
+    dataset: "production",
+  },
+  studioHost: "kleinhansdigital",
+});
