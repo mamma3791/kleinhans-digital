97293028bcbd348f6f8d64d3ad585548b3041434
mamma3791 <mamm3791@gmail.com>
2026-04-11 22:29:41 +0200

    Remove sanity.cli.ts from Next.js repo

--- sanity.cli.ts
+++
@@ -1,9 +0,0 @@
-import { defineCliConfig } from "sanity/cli";
-
-export default defineCliConfig({
-  api: {
-    projectId: "z24f1kn9",
-    dataset: "production",
-  },
-  studioHost: "kleinhansdigital",
-});
